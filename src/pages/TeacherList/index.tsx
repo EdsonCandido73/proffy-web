58cf028a0faa5135afbffd20f26d8c6a39794353
Edson Candido <edson.cursoprogramacao@gmail.com>
2020-08-04 21:33:30 -0300

    Conclusão da página de listagem de Proffys

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
-import {  Link } from 'react-router-dom';
+
+import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
 
 import './styles.css';
-
-import logoImg from '../../assets/images/logo.svg';
-import  backIcon from '../../assets/images/icons/back.svg';
 
 function TeacherList() {
     return (
         <div id="page-teacher-list" className="container">
-            <header className="page-header">
-                <div className="top-bar-container">
-                    <Link to="/">
-                        <img src={backIcon} alt="Voltar"/>
-                    </Link>
-                    <img src={logoImg} alt="Proffy"/>
-                </div>
+            <PageHeader title="Estes são os proffys disponíveis.">
+                <form id="search-teachers">
+                    <div className="input-block">
+                        <label htmlFor="subject">Matéria</label>
+                        <input type="text" id="subject"/>
+                    </div>
 
-                <div className="header-content">
-                    <strong>Estes são os proffys disponíveis</strong>
-                </div>
-            </header>
+                    <div className="input-block">
+                        <label htmlFor="week_day">Dia da semana</label>
+                        <input type="text" id="week_day"/>
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="time">Hora</label>
+                        <input type="text" id="time"/>
+                    </div>
+                </form>
+            </PageHeader>
+
+            <main>
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+            </main>
         </div>
     )
 }
